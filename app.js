9eff1eb95ea1ba323758654faebfc90ef8a0beae
melwell027 <melwell1027@Gmail.com>
2018-09-20 16:34:47 -0500

    made changes after tutoring by Sue

--- app.js
+++ app.js
@@ -12,11 +12,15 @@ var words = [
     "INDIANAPOLIS"
 ];
 
-
 // var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-var guessesRemaining = 10;
+var guessesRemaining = 0;
+console.log("gr", guessesRemaining);
 var wins = 0;
+var userGuess = "";
+var asterisk = [];
+var splitWord = [];
+var guessedLetters = [];
 
 // Create variables that hold references to the places in the HTML where we want to display things.
 var currentWordText = document.getElementById("currentword-text");
@@ -24,79 +28,80 @@ var guessesRemainingText = document.getElementById("guessesRemaining");
 var guessesText = document.getElementById("guesses");
 var winsText = document.getElementById("winsText");
 
-
-// choose random word
-var computerWord = words[Math.floor(Math.random() * words.length)];
-console.log(computerWord);
-
-var splitWord = computerWord.split('');    
-
-
-var asterisk = []
-
-
-
-
-for(var i = 0; i < splitWord.length; i++){
-    asterisk[i] = "* ";
-}
-
-if (guessesRemaining === -1){
-    alert("You Lose!");
+function startGame(){
+    // choose random word
+    var computerWord = words[Math.floor(Math.random() * words.length)];
+    console.log(computerWord);
+    
+    splitWord = computerWord.split('');    
+    
+    guessesRemaining = 2;
+    asterisk = [];
+    guessedLetters = [];
+    guessesText.textContent = "";
+    
+    for(var i = 0; i < splitWord.length; i++){
+        asterisk[i] = "* ";
+    }
+    
 }
 
 // check to see if user user guess is a letter in the computer word
 
-function letterCheck () {
-    var userGuess = event.key.toUpperCase();
-    for (i = 0; i < splitWord.length; i++){
-        if (userGuess === splitWord[i]){
-            console.log("hello"); // replace matched characters
-            asterisk[i] = splitWord[i];
+function checkGuessedArray() {
+    console.log("uG in CGA", userGuess);
+    if (guessedLetters.indexOf(userGuess) === -1) {
+        guessedLetters.push(userGuess);
+        guessesText.textContent += (userGuess + " ");
+        console.log("gl", guessedLetters);
+        letterCheck();
+    } else {
+        alert("You have already guessed this letter. Try again!");
+    }
+}
+
+function letterCheck() {
+    if (splitWord.indexOf(userGuess) === -1) {
+        guessesRemaining--;
+        if (guessesRemaining === -1) {
+            alert("You Lose!");
+            startGame();
+        }
+    } else {
+        for (i = 0; i < splitWord.length; i++) {
+            if (userGuess === splitWord[i]) {
+                console.log("hello"); // replace matched characters
+                asterisk[i] = splitWord[i];
+                console.log(splitWord.join(""), asterisk.join(""));
+                if (splitWord.join("") == asterisk.join("")){
+                    console.log("in wins");
+                    wins++;
+                    alert("You win!");
+                    startGame();
+                }
+            }
         }
     }
 }
-
-
-function reduceGuesses(){
-// for (i = 0; i < guessesRemaining.length; i--);
-    guessesRemaining--;
-    if (guessesRemaining === 0){
-        alert("You lose!");
-    }
-}
-
-
-// create a variable to store user input
-document.onkeyup = function(event){
-    pressAny.innerHTML = "<br>";
-    
-    
-    
+startGame();    
+    // create a variable to store user input
+    document.onkeyup = function(event){
+        pressAny.innerHTML = "<br>";
+        userGuess = event.key.toUpperCase();
+        console.log("userGuess", userGuess);
     
     // check to see if letter exists in array
-    
-    letterCheck();
-    reduceGuesses();
-    
-    
+    checkGuessedArray();
+    //letterCheck();
     
     
     // create variable for guesses remaining
     
-    
-    
-    
-    
     // Display the randomly chosen word on screen
-    guessesText.textContent += (event.key + " ");
     currentWordText.textContent = asterisk.join("");
     guessesRemainingText.textContent = guessesRemaining;
     winsText.textContent = wins;
 }
-
-
-
 
  
 // Reduce guesses by one per guess
